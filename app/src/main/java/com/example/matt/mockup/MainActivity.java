812dbc8d4f106b01fd2d730102d8f118c95870d0
Mathew Punsalan <Mattpunsalan@gmail.com>
2018-10-14 14:59:18 -0700

    broken login screen layout due to old linear layout

--- app/src/main/java/com/example/matt/mockup/MainActivity.java
+++ app/src/main/java/com/example/matt/mockup/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.matt.mockup;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConstraintLayout constraintLayout = findViewById(R.id.layout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        RelativeLayout relativeLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
